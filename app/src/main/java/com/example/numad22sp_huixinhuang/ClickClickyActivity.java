9ed8448f3139f54f98772ef7dcb485daa98761b6
Hailey Huang <huang.huix@northeastern.edu>
2022-01-31 20:24:42 -0800

    This is the second commit for Assignment3. Has added activities for 6 buttons.

--- app/src/main/java/com/example/numad22sp_huixinhuang/ClickClickyActivity.java
+++ app/src/main/java/com/example/numad22sp_huixinhuang/ClickClickyActivity.java
@@ -3,12 +3,69 @@ package com.example.numad22sp_huixinhuang;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class ClickClickyActivity extends AppCompatActivity {
+
+    private Button buttonA;
+    private Button buttonB;
+    private Button buttonC;
+    private Button buttonD;
+    private Button buttonE;
+    private Button buttonF;
+    private TextView message;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_click_clicky);
+        message = (TextView) findViewById(R.id.textMessage);
+        buttonA = (Button)findViewById(R.id.buttonA);
+        buttonA.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                message.setText("Pressed: A");
+            }
+        });
+        buttonB = (Button)findViewById(R.id.buttonB);
+        buttonB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                message.setText("Pressed: B");
+            }
+        });
+        buttonC = (Button)findViewById(R.id.buttonC);
+        buttonC.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                message.setText("Pressed: C");
+            }
+        });
+        buttonD = (Button)findViewById(R.id.buttonD);
+        buttonD.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                message.setText("Pressed: D");
+            }
+        });
+        buttonE = (Button)findViewById(R.id.buttonE);
+        buttonE.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                message.setText("Pressed: E");
+            }
+        });
+        buttonF = (Button)findViewById(R.id.buttonF);
+        buttonF.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                message.setText("Pressed: F");
+            }
+        });
+
+
     }
+
 }
